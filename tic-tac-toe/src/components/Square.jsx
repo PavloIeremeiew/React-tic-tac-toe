9c550613e396aac6115a3aea5fadc0342d1968dd
Pavlo Yeremeiew <113441856+PavloIeremeiew@users.noreply.github.com>
2024-07-22 10:11:21 +0300

    style/ add style to board

--- tic-tac-toe/src/components/Square.jsx
+++ tic-tac-toe/src/components/Square.jsx
@@ -1,5 +1,6 @@
 function Square({ value, onClick }) {
-  return <button className="square" onClick={onClick}>{value} </button>;
+  const buttonColor = value==="x"? "#df1515" : value==="o"? "#007bff" :"#000000";
+  return <button style={{ color: buttonColor }} className="square" onClick={onClick}>{value} </button>;
 }
 
 export default Square;
